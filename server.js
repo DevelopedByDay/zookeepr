36803edf2f6d91c01d73a6019d1e91d959e2f4b5
BradD <bradleyeday@gmail.com>
2020-08-04 10:31:13 -0500

    added validation

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { animals } = require('./data/animals.json')
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 //parse incoming string or array data
@@ -52,6 +54,34 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+        );
+
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -72,7 +102,20 @@ app.get('/api/animals/:id', (req, res) => {
 app.post('/api/animals', (req, res) => {
     // req.body is where our incoming content will be
     console.log(req.body);
-    res.json(req.body);
+
+    req.body.id = animals.length.toString();
+
+    // validation
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    }
+    else {
+        const animal = createNewAnimal(req.body, animals);
+
+        res.json(animal);
+    }
+
+    
 });
 
 app.listen(PORT, () => {
